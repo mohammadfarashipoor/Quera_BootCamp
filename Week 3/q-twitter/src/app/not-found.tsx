5dbccd37269cd0bfe74365916ec6b36f1bb58e10
Mohammad <mohammad1f2f@gmail.com>
2024-06-10 18:47:15 +0330

    feat 🕊 : not found page

---
+++ Week 3/q-twitter/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <div className="h-screen flex justify-center items-center text-white">
+      <span>صفحه مورد نظر یافت نشد</span>
+    </div>
+  );
+}
+
+export default NotFound;
